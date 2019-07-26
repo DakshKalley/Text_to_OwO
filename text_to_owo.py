1074ad852aa461a468b86abfe1c73483a6e46101
DakshKalley <dakshk117@gmail.com>
2019-07-26 20:00:07 +0530

    Updated program to enable user text input

--- text_to_owo.py
+++ text_to_owo.py
@@ -1,17 +1,21 @@
-#0w0?
+# 0w0?
 
 choice = input("Would you like to convert your text? (Y/N) ")
 
-if choice.lower() == "y":
-    passage = open(input("Enter the location of your text here: "))
-    owo = passage.read()
+if choice.lower() == ("y"):
+    choiceb = input("Do you want to enter the text? (Y/N) ")
+    if choiceb.lower() == ("y"):
+        owo = input("Enter text here: ")
+    else:
+        passage = open(input("Enter the location of your text here: "))
+        owo = passage.read()
 
     def owoconvert(text):
         text = text.replace('L', 'W').replace('l', 'w')
         text = text.replace('R', 'W').replace('r', 'w')
-        print ('\n' + text)
+        print('\n' + text)
 
     owoconvert(owo)
 
 else:
-    print ("(´・ω・\`)")
+    print("(´・ω・`)")
